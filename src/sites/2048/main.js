d9ae78efc5ea09085123d5047474e385c92b0b41
benbenye <1203846281@qq.com>
2019-07-23 11:28:09 +0800

    :heavy_plus_sign: add vconsole to debug

--- src/sites/2048/main.js
+++ src/sites/2048/main.js
@@ -1,3 +1,4 @@
+import vConsole from 'vconsole';
 import 'amfe-flexible';
 import Vue from 'vue';
 import App from './App.vue';
@@ -5,6 +6,7 @@ import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
+new vConsole();
 new Vue({
   render: h => h(App)
 }).$mount('#app');
