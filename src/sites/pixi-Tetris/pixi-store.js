befe97b31aee954626bafa32bcc13e9c58907977
benbenye <1203846281@qq.com>
2019-08-14 14:36:23 +0800

    :tada: add game store

--- src/sites/pixi-Tetris/pixi-store.js
+++ src/sites/pixi-Tetris/pixi-store.js
@@ -6,11 +6,11 @@ initStore();
 export function initStore() {
   data.boxId = 'tetris-bg';
   data.docId = 'tetris';
-  data.delay = 100;
+  data.delay = 1000;
   data.currentScore = 0;
   data.highestScore = 0;
   data.isGameOver = false;
-  data.guide = false;
+  data.guide = true;
 }
 
 export default data;
