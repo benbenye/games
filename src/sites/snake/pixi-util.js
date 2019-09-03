d0f786e64c01929aa8fb86ce35068ac52e614990
benbenye <1203846281@qq.com>
2019-09-03 10:18:23 +0800

    :hammer: add transform x y

--- src/sites/snake/pixi-util.js
+++ src/sites/snake/pixi-util.js
@@ -2,3 +2,9 @@ export function strip(num, precision = 12) {
   return +parseFloat(num.toPrecision(precision));
 }
 
+export function transform (num, xCoo) {
+  return {
+    y: _.floor(num / xCoo),
+    x: num % xCoo
+  };
+}
